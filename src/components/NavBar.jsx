40f6219dcf3e7cf28bc285abd5212154a650b9a0
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-09 00:13:49 +0200

    add tab bar with login and register tabs

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
 import { Link, withRouter } from "react-router-dom";
 import { Dropdown, DropdownButton, Modal } from "react-bootstrap";
+import { Tab, Tabs } from "react-bootstrap";
 
 class NavBar extends React.Component {
   constructor(props) {
@@ -34,34 +35,40 @@ class NavBar extends React.Component {
 
   render() {
     return (
-      <Navbar bg="light" variant="light" expand="md">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Link to="/">Calculators</Link>
-          <Link to="/about">About</Link>
-        </Nav>
-        {this.props.location.pathname === "/" && (
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-primary">Search</Button>
-          </Form>
-        )}
-        <DropdownButton
-          alignRight
-          title="Account"
-          id="dropdown-menu-align-right"
-        >
-          <Button variant="light" onClick={this.handleClick}>
-            Sign in
-          </Button>{" "}
-          <Button variant="dark" onClick={this.handleClick}>
-            Join
-          </Button>{" "}
-          <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
-          <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
-        </DropdownButton>
+      <div>
+        <Navbar bg="light" variant="light" expand="md">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Link to="/">Calculators</Link>
+            <Link to="/about">About</Link>
+          </Nav>
+          {this.props.location.pathname === "/" && (
+            <Form inline>
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2"
+              />
+              <Button variant="outline-primary">Search</Button>
+            </Form>
+          )}
+          <DropdownButton
+            alignRight
+            title="Account"
+            id="dropdown-menu-align-right"
+          >
+            <Button variant="light" onClick={this.handleClick}>
+              Sign in
+            </Button>{" "}
+            <Button variant="dark" onClick={this.handleClick}>
+              Join
+            </Button>{" "}
+            <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+            <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+          </DropdownButton>
+        </Navbar>
         <Modal
           show={this.state.showModal}
           onHide={this.onHide}
@@ -75,18 +82,21 @@ class NavBar extends React.Component {
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <h4>Centered Modal</h4>
-            <p>
-              Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-              dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-              ac consectetur ac, vestibulum at eros.
-            </p>
+            <h4>Studentko</h4>
+            <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+              <Tab eventKey="home" title="Register">
+                RegisterButton
+              </Tab>
+              <Tab eventKey="profile" title="Login">
+                LoginButton
+              </Tab>
+            </Tabs>
           </Modal.Body>
           <Modal.Footer>
             <Button onClick={this.onHide}>Close</Button>
           </Modal.Footer>
         </Modal>
-      </Navbar>
+      </div>
     );
   }
 }
