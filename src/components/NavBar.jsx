c3d9d594a17ed46aa0f5fe51db48801452e2ca4a
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-08-08 12:13:12 +0200

    add account dropdown button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { Dropdown, DropdownButton } from "react-bootstrap";
 
 export default class NavBar extends React.Component {
   render() {
@@ -19,6 +20,17 @@ export default class NavBar extends React.Component {
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
           <Button variant="outline-primary">Search</Button>
         </Form>
+        <DropdownButton
+          alignRight
+          title="Account"
+          id="dropdown-menu-align-right"
+        >
+          <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+          <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+          <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+        </DropdownButton>
       </Navbar>
     );
   }
